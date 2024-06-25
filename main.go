01d298ed6b574501a2210dd7282a3068e31a5dc4
AgolaDeveloper <owinoagola00@gmail.com>
2024-06-25 23:25:32 +0300

    Practised SEARCH Operation on Arrays

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import "fmt"
 
+func advancedArray() {
+	//trying to do a SEARCH OPERATION on our array
+	fmt.Printf("***************************************************\n**************************************************\n\n")
+	fmt.Println("Beginning of our ADVANCED ARRAY SECTION")
+
+	ourArray := [10]int{}
+
+	//and every element in our array'd be double its previous contagious element
+	//...where the first element is 4
+	ourArray[0] = 2
+	for i := 1; i < len(ourArray); i++ {
+		ourArray[i] = ourArray[i-1] * 2
+	}
+
+	fmt.Printf("OUR ARRAY: \n      %v", ourArray)
+}
+
 func main() {
 	LearningGo := "My First Commit:"
 
-	fmt.Printf("I'm passionate about Go. Just getting started \n Here's %v", LearningGo)
+	fmt.Printf("I'm passionate about Go. Just getting started \n Here's %v\n", LearningGo)
+
+	fmt.Println()
 
 	//Go-Slices
 	//MySlice()
-	advanceArray()
+	advancedArray()
+
 }

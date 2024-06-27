28ec0e5a46499871d68bd92342ace5f9eb3ca59d
AgolaDeveloper <owinoagola00@gmail.com>
2024-06-27 12:52:06 +0300

    Adding functions to help with Temp conversion

---
+++ miniProjects/TempConverter.go
@@ -0,0 +1,74 @@
+package main
+
+//here WE HELP USERS WITH TEMP-CONVERSION
+//USER MUST ENTER THE TEMP AND TARGET TEMP
+import "fmt"
+
+//functions that help with the conversions
+//...both take two parameters from user;
+//userTemp and Target temp...plus
+//a third constant parameter
+
+//this helps with converting Celsius->Fah
+
+func conCelsius(userTemp, k float64) {
+	finalTemp := userTemp * k
+
+	fmt.Printf("%v C -> Fah: %v \n", userTemp, finalTemp)
+}
+
+//this helps with converting Fah->Celsius
+func conFah(userTemp, k float64) {
+	finalTemp := userTemp / k
+
+	fmt.Printf("%v Fah -> C: %v \n", userTemp, finalTemp)
+}
+
+func main() {
+
+	//formular for Celcius->Fah Conversion
+	//... Fah=celcius*121.63
+
+	//formular for Fah->Celcius Conversion
+	//... Cel=Fah/121.63
+
+	const k = 121.63
+
+	var yourTemp float64
+	var conversionFit int
+
+	//user will do the following:
+	//1. Enter the Temp they wanna convert [yourTemp]
+	//2. Enter the target Temp they need their original temp converted to
+
+	//3. finally trigger display of the conversion/result
+
+	//FIRST MAKING IT EASY FOR USER TO NAVIGATE U/I
+
+convert:
+	fmt.Printf("\nCHOOSE THE BEST FIT FOT YOUR CONVERSION\n ************************\n")
+	fmt.Println("1. Celcius to Fah")
+	fmt.Println("2. Fah to Celcius")
+
+	fmt.Scan(&conversionFit)
+
+	//then we use switch
+	switch conversionFit {
+	case 1:
+		fmt.Println("ENTER Temp You wanna Convert (C): ")
+		fmt.Scan(&yourTemp)
+
+		conCelsius(yourTemp, k)
+
+	case 2:
+		fmt.Println("ENTER Temp You wanna Convert (Fah): ")
+		fmt.Scan(&yourTemp)
+
+		conFah(yourTemp, k)
+
+	default:
+		fmt.Println("INVALID CHOICE: Retry....")
+		goto convert
+	}
+
+}

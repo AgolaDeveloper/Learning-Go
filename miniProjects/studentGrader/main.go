9766db2f0398085f6d563647bdd94a1e1cf48b08
AgolaDeveloper <owinoagola00@gmail.com>
2024-06-27 20:58:36 +0300

    Defining instances of our 2 Data STRUCTS & setting data to the STRUCTS through their local objects/instance

--- miniProjects/studentGrader/main.go
+++ miniProjects/studentGrader/main.go
@@ -3,13 +3,15 @@
 
 package main
 
+import "fmt"
+
 //WE first create a composite/complex data type that'll store every detail of a student
 
 type Students struct {
-	firstName string
-	lastName  string
-	age       int
-	subjects  []map[string]int
+	firstName       string
+	lastName        string
+	admissionNumber string
+	subjects        map[string]int
 }
 
 //SETTER methods that help with setting items to every student of instance STUDENTS STRUCT data type
@@ -22,11 +24,11 @@ func (s *Students) setLastName(lname string) {
 	s.lastName = lname
 }
 
-func (s *Students) setAge(ag int) {
-	s.age = ag
+func (s *Students) setAdmission(admissionNumber string) {
+	s.admissionNumber = admissionNumber
 }
 
-func (s *Students) setSubjects(subjects []map[string]int) {
+func (s *Students) setSubjects(subjects map[string]int) {
 	s.subjects = subjects
 }
 
@@ -40,11 +42,11 @@ func (s Students) getLastName() string {
 	return s.lastName
 }
 
-func (s Students) getAge() int {
-	return s.age
+func (s Students) getAdmission() string {
+	return s.admissionNumber
 }
 
-func (s Students) getSubjects() []map[string]int {
+func (s Students) getSubjects() map[string]int {
 	return s.subjects
 }
 
@@ -81,5 +83,123 @@ func (c *Class) getStudents() []Students {
 //Program's Entry Point
 
 func main() {
+
+	//DEFINING LOCAL VARIABLES FOR OUR composite Data Structure
+
+	//beginning with local variables for Students STRUCT
+
+	var firstName string
+	var lastName string
+	var admissionNumber string
+	subjects := make(map[string]int)
+
+	//...then we move to defining local variables for Class Struct
+	var className string
+	students := make([]Students, 0)
+
+	//creating instances of all our defined Data structs
+	//begin with instance of Students
+	student := Students{}
+
+	//then instance of Class
+	class := Class{}
+
+	//we'll now begin setting items to our Class object
+	//it should have its name
+	//...and a slice of Student objects, as its data elements
+
+	fmt.Println("Welcome to our Skoolie App")
+	//user has some options that help in navigating this application's features
+	//__ 1. Record Students Details/Grades
+	//__ 2.
+
+	fmt.Printf("Record Students' Details/Grades\n STUDENTS FROM YOUR CLASS ONLY\n")
+	fmt.Println("Enter Class [Name of Your Class]: ")
+	fmt.Scan(&className)
+	class.setClassName(className)
+
+	//before we set/store students, we gotta set values/items of Student, for every student we haave in this class
+	var numOfStudents int
+	fmt.Println("How many students do you wanna record their details?")
+	fmt.Scan(&numOfStudents)
+
+	//now we're going to set details for the n-number of students fed in by the teachers/user above
+
+	for i := 0; i < numOfStudents; i++ {
+		fmt.Printf("You Ready to Record details for STUDENT %v? \n CONTINUE>>\n", (1 + i))
+
+		fmt.Println("Enter Student's First Name: ")
+		fmt.Scan(&firstName)
+		student.setFirstName(firstName)
+
+		fmt.Println("Enter Student's Last Name: ")
+		fmt.Scan(&lastName)
+		student.setLastName(lastName)
+
+		fmt.Println("Enter Student's Admission Number: ")
+		fmt.Scan(&admissionNumber)
+		student.setAdmission(admissionNumber)
+
+		//for every student, you'll have to record subjects and marks scored
+		//teacher/user must map every subject to a respective mark scored by the student
+
+		//but how many subjects Did the students tackle?
+
+		var numOfSubjects int
+
+		fmt.Printf("\nHow Many Subjects for %v's Records/Ranking? \n >>: \n", student.getLastName())
+		fmt.Scan(&numOfSubjects)
+
+		var subject string
+		var marks int
+
+		for i := 0; i < numOfSubjects; i++ {
+			fmt.Printf("Enter %v's Subject-%v: ", student.lastName, i+1)
+			fmt.Scan(&subject)
+
+			fmt.Printf("Enter %v's Mark-Score: ", subject)
+			fmt.Scan(&marks)
+
+			//every iteration...recording of a subject and respective mark-Score must be mapped
+			subjects[subject] = marks
+
+			//once it's mapped we need to set IT TO THE struct Student's subject item
+			//student.setSubjects(subjects)
+		}
+		//then set marks the student has scored to our data struct of Students
+		student.setSubjects(subjects)
+
+		//then we store every instance of Student [as object element]in the Class-slice of object Students of the class
+
+		//thus we'll append OBJECT STUDENT to the Class's local slice of Students in every iteration
+		//...[every iteration sets an instance/object of Students completely]
+
+		students = append(students, student)
+
+		//we, just appended [1] object of type Student to Struct Class's slice of struct-objects
+		//... to store it to the struct we need to set it
+
+		class.setStudents(students)
+	}
+	//class.setStudents(students)
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	//this section helps to access data elements inn both the 2 Structs
+	//STARTING WITH DATA ELEMENETS STORED IN struct Class
+
+	//__1. let's access name of our class
+	classNam := class.getClassName()
+
+	fmt.Printf("Class-Name: %v \n", classNam)
+
+	//__2. let's access students in this class
+	classStudents := class.getStudents()
+
+	fmt.Println("Class-Students: ")
+	for _, value := range classStudents {
+		fmt.Println(value)
+	}
+	//fmt.Println("Welcome to our SKoolie App")
 
 }
